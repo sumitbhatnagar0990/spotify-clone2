f534e0b0024bfb24dd1cf64b3fadb59fc3a41baa
Sumit Bhatnagar <sumitbhatnagar0990@gmail.com>
2025-10-05 02:50:12 +0530

    Fixed fetch path for deployment

--- script.js
+++ script.js
@@ -37,7 +37,8 @@ volumeSlider.addEventListener("input", (e) => {
 
 
  async function getSongs(){
-  let a = await fetch("/songs/")
+  let a = await fetch("songs/")
+
 
   let response = await a.text();
   let div=document.createElement("div")
